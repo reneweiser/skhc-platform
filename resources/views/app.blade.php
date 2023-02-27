b8f58c866aa7636315c41371c3e6f0c91c1989ed
reneweiser <reneweiser@gmail.com>
2023-02-27 17:13:28 +0100

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,9 @@
 
         <!-- Scripts -->
         @routes
+        <script>
+            Ziggy.url = '{{ env('APP_URL') }}';
+        </script>
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
