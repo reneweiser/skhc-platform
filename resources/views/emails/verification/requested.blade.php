d43c8baead88194a757d63094e17b725b670ef15
reneweiser <reneweiser@gmail.com>
2023-03-08 21:44:21 +0100

    Updated verification emails

--- resources/views/emails/verification/requested.blade.php
+++ resources/views/emails/verification/requested.blade.php
@@ -1,3 +1,20 @@
+<p class="italic font-bold">
+    Bitte antworte nicht auf diese Email. Du möchtest uns schreiben, schreib an skhc@kulturtragwerk.de.
+</p>
+
 <p>
-    Bitte klicke den nachfolgenden Link: {{ $link }}
+    Bitte klicke den nachfolgenden Bestätigungslink um deine Registrierung abzuschließen:<br />
+    {{ $link }}
+</p>
+
+<p>
+    Wir freuen uns und möchten uns für deine Hilfe bedanken.
+</p>
+
+<p>
+    Dein SKHC-Orgateam.
+</p>
+
+<p>
+    PS: Deine Registrierung kann mit dem Bestätigungslink bis {{now()->addHours(6)->toDateTimeString()}} abgeschlossen werden. Wenn die Bestätigung nicht erfolgt, werden deine gelöscht.
 </p>
--- resources/views/emails/verification/successful.blade.php
+++ resources/views/emails/verification/successful.blade.php
@@ -1,8 +1,28 @@
-<h1>Hi {{$volunteer->first_name}},</h1>
+<p class="italic font-bold">
+    Bitte antworte nicht auf diese Email. Du möchtest uns schreiben, schreib an skhc@kulturtragwerk.de.
+</p>
+
+<h1>Hallo {{$volunteer->first_name}},</h1>
 
 <p>
-    deine Anmeldung war erfolgreich! Wir freuen uns schon dich kennen zu lernen.
+    deine Anmeldung war erfolgreich!
 </p>
+
+<p>
+    Wir freuen uns dich als Helfer*in schon bald kennen zu lernen. Kurz vor dem Rennen mit der anschließenden Jubelfeier am 1. Mai werden wir mit dir in Kontakt treten. Dann gibt es detaillierte Informationen zu deiner Schicht.
+</p>
+
+<p>
+    Das sind deine persönlichen Daten. Bitte kontrolliere deine Mobilfunknummer, sodass wir dich erreichen können.
+</p>
+
+<ul>
+    <li>Vorname: {{$volunteer->first_name}}</li>
+    <li>Nachname: {{$volunteer->last_name}}</li>
+    <li>Email: {{$volunteer->email}}</li>
+    <li>Mobile: {{$volunteer->mobile}}</li>
+    <li>T-Shirt Größe: {{$volunteer->shirtSize->name}}</li>
+</ul>
 
 <p>
     Hier sind die Schichten, für die du dich angemeldet hast.
@@ -15,7 +35,7 @@
 </ul>
 
 <p>
-    Dein T-Shirt kannst du dir im //LOCATION// abholen.
+    Dein Helfer-Tshirt kannst du dir am Orgazelt an der Rennstrecke abholen! Falls du es nicht schaffst, schreib uns eine mail und wir vereinbaren ein Treffen.
 </p>
 
 <p>
@@ -23,14 +43,14 @@
 </p>
 
 <p>
-    Wenn du weitere Fragen hast, lass sie unser <a href="mailto:organisation@orga-skhc.de">Orga-Team</a> wissen.
+    Wenn du weitere Fragen hast, kontaktiere uns - <a href="mailto:skhc@kulturtragwerk.de">skhc@kulturtragwerk.de</a>.
 </p>
 
 <p>
-    Ansonsten sehen wir uns beim Rennen!
+    Ansonsten sehen wir uns beim Rennen und spätestens zur Jubelfeier!
 </p>
 
 <p>
     Viele Grüße,<br>
-    SKHC Orga
+    Das SKHC-Orgateam
 </p>
