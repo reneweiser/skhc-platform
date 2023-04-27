a563d0dc1c3f28fcfcc5b162a78c262d47b59493
René Weiser <rene@encoding.group>
2023-04-27 12:18:01 +0200

    Update Index.vue

--- resources/js/Pages/Shifts/Index.vue
+++ resources/js/Pages/Shifts/Index.vue
@@ -132,7 +132,14 @@ function handleDelete() {
                 <span class="font-bold">alle Schichten</span>
                 unwiderruflich löschen willst?
             </p>
-            <div class="flex justify-end">
+            <div class="flex justify-end space-x-4">
+                <button
+                    type="button"
+                    @click="showDeleteModal = false"
+                    class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                >
+                    Nein
+                </button>
                 <button
                     type="submit"
                     @click="handleDelete"
