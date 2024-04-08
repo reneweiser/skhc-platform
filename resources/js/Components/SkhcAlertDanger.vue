3d20588bdfc8bcc616db151d30716eac9ceb52b1
René Weiser <hello@reneweiser.de>
2024-04-08 11:16:54 +0200

    Made deleting Shifts more obvious

---
+++ resources/js/Components/SkhcAlertDanger.vue
@@ -0,0 +1,16 @@
+<script setup>
+import VueFeather from 'vue-feather';
+</script>
+
+<template>
+    <div
+        class="flex items-center p-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50"
+        role="alert"
+    >
+        <VueFeather
+            class="flex-shrink-0 inline w-4 h-4 me-3"
+            type="alert-triangle"
+        />
+        <slot />
+    </div>
+</template>
--- resources/js/Components/SkhcShiftDeleteForm.vue
+++ resources/js/Components/SkhcShiftDeleteForm.vue
@@ -3,6 +3,7 @@ import { useForm } from '@inertiajs/vue3';
 import SkhcButtonDanger from '@/Components/SkhcButtonDanger.vue';
 import SkhcButtonMuted from '@/Components/SkhcButtonMuted.vue';
 import { ref } from 'vue';
+import SkhcAlertDanger from '@/Components/SkhcAlertDanger.vue';
 
 const props = defineProps({
     shift: { type: Object, required: true },
@@ -22,27 +23,29 @@ function submit() {
 <template>
     <div>
         <div v-if="!isReady">
-            <SkhcButtonDanger
+            <button
                 @click="isReady = true"
+                class="text-red-400 underline"
                 type="button"
             >
                 Löschen
-            </SkhcButtonDanger>
+            </button>
         </div>
-        <form
-            @submit.prevent="submit"
-            class="border border-red-400 p-2 rounded-lg flex justify-between items-center space-x-8"
-            v-if="isReady"
-        >
-            <div>Bist du sicher?</div>
-            <div>
-                <SkhcButtonMuted
-                    @click="isReady = false"
-                    type="button"
-                    >Abbrechen</SkhcButtonMuted
-                >
-                <SkhcButtonDanger> Ja </SkhcButtonDanger>
-            </div>
-        </form>
+        <SkhcAlertDanger v-if="isReady">
+            <form
+                @submit.prevent="submit"
+                class="w-full flex justify-between items-center space-x-8"
+            >
+                <div>Bist du sicher?</div>
+                <div>
+                    <SkhcButtonMuted
+                        @click="isReady = false"
+                        type="button"
+                        >Nein</SkhcButtonMuted
+                    >
+                    <SkhcButtonDanger> Ja </SkhcButtonDanger>
+                </div>
+            </form>
+        </SkhcAlertDanger>
     </div>
 </template>
